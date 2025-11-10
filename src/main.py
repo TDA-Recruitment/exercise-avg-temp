5f7c0831c840773b6bffbae95032221ceddb9caa
Michael Shapiro <michaels@tripletech.co.il>
2025-11-10 15:03:09 +0200

    added an empty class

---
+++ src/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+class HomeworkClient:
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+
+if __name__ == "__main__":
+    client = HomeworkClient()
+    client.run()
